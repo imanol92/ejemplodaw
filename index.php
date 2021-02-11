b79fb230860c1a880934521a5a61e07696f64cf4
Imanol Delgado <imanol9.92@gmail.com>
2021-02-11 11:56:19 +0100

    Archivos

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+
+try {
+require __DIR__ . '/vendor/autoload.php';
+
+//librería para poder leer archivos.env
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+try {
+    $pdo = new \PDO($_ENV['DB_CONNECTION'] . ":host=" . 
+        $_ENV['DB_HOST'].";port=".$_ENV['DB_PORT'] . ";dbname=" . $_ENV['DB_DATABASE'], 
+        $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD']);
+    $pdo->exec("set names utf8");
+    $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Exception PDO";
+    echo $e->getMessage();
+    exit();
+}
+
+// $sql = "SELECT * FROM personas WHERE id = :id";
+$sql = "SELECT * FROM personas";
+
+$params = [
+    ":id" => 2
+];
+
+$ps = $pdo->prepare($sql);
+$ps->execute($params);
+$result = $ps->columnCount()>0? $ps->fetchAll(\PDO::FETCH_ASSOC): $ps->rowCount(); 
+
+echo '<h1>resultado</h1>';
+// var_dump($result);
+
+foreach ($result as $key => $value) {
+    foreach ($value as $ky => $val) {
+        echo $ky . ": " . $val . " ";
+    }
+    echo "<br>";
+}
+
+
+
+} catch (Exception $e) {
+    echo "Exception";
+    echo $e->getMessage();
+}
